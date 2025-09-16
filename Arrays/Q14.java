6a3167d7931a58754c4e97a28ec1f651a5f31482
Sangram Khandagale <khandagalesangram26@gmail.com>
2025-09-16 20:52:03 +0530

    Streak continues

---
+++ Arrays/Q14.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Q14{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+
+        int j=0;
+
+        for(int i=0;i<n;i++){
+            
+            if(arr[i]!=0){
+             arr[j]=arr[i];
+             j++;
+            }
+        }
+
+        while(j<n){
+            arr[j]=0;
+            j++;
+        }
+
+        for(int x:arr){
+            System.out.println(x);
+        }
+    }
+}
---
+++ Arrays/Q15.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Q15{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter array size:");
+        int n=in.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter array:");
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+
+        System.out.println("Enter rotations:");
+        int rotate=in.nextInt();
+
+        for(int x=0;x<rotate;x++){
+            int temp=arr[arr.length-1];
+           for(int i=arr.length-2;i>=0;i--){
+             arr[i+1]=arr[i];
+           }
+           arr[0]=temp;
+        }
+
+        System.out.println("Result:-");
+        for(int x:arr){
+            System.out.println(x);
+        }
+    }
+}
