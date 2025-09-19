6c2913614503a6bdd18007303aa24cdc460fcea5
Sangram Khandagale <khandagalesangram26@gmail.com>
2025-09-19 19:52:19 +0530

    Streak continues

---
+++ Arrays/Q18.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Q18{
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(); // value -> index
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{ map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{}; // never reached if solution guaranteed
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        int[] ans = twoSum(arr, target);
+        System.out.println("Indices: " + ans[0] + ", " + ans[1]);
+        sc.close();
+    }
+}
