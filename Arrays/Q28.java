654c6b8030067918e56d2131b87132e2c8219374
Sangram Khandagale <khandagalesangram26@gmail.com>
2025-10-03 20:39:59 +0530

    Prefix trie

---
+++ Arrays/Q28.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Q28{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        in.nextLine();
+        String[] arr=new String[n];
+        for(int i=0;i<n;i++){
+         arr[i]=in.nextLine();
+        }
+
+        String k=in.nextLine();
+        
+
+        for(int j=0;j<n;j++){
+            int count=0;
+            for(int i=0;i<k.length();i++){
+                if(arr[j].charAt(i)==k.charAt(i)){
+                    count++;
+                }
+            }
+
+            if(count==k.length()){
+                System.out.println(arr[j]);
+            }
+        }
+    }
+}
