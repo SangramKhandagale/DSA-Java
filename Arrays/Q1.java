d28bcd9c743c08c1f540e01dc98c4505179238ca
Sangram Khandagale <160144801+SangramKhandagale@users.noreply.github.com>
2025-10-07 18:28:02 +0530

    Update Q1.java

--- Arrays/Q1.java
+++ Arrays/Q1.java
@@ -5,6 +5,7 @@ public class Q1{
         Scanner in=new Scanner(System.in);
         int n=in.nextInt();
         int arr[]=new int[n];
+        
         for(int i=0;i<n;i++){
             arr[i]=in.nextInt();
         }
