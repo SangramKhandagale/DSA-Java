61a8b1b868979201c341ecffaf10f95254aa0ded
Sangram Khandagale <160144801+SangramKhandagale@users.noreply.github.com>
2025-10-12 16:28:47 +0530

    Update Q1.java

--- Arrays/Q1.java
+++ Arrays/Q1.java
@@ -6,6 +6,7 @@ public class Q1{
         int arr[]=new int[n];
         
         for(int i=0;i<n;i++){
+            
             arr[i]=in.nextInt();
         }
        System.out.println("Largest number: "+max(arr));
