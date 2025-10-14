4be3ff49cb89d4a3b2627b73a5e16deb339d2640
Sangram Khandagale <160144801+SangramKhandagale@users.noreply.github.com>
2025-10-14 20:49:24 +0530

    Update Q10.java

--- Arrays/Q10.java
+++ Arrays/Q10.java
@@ -16,6 +16,7 @@ public class Q10 {
         for (int i = 0; i < n; i++) {
             int count = 0;
             for (int j = 0; j < n; j++) {
+                
                 if (arr[i] == arr[j]) {
                     count++;
                 }
