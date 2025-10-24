9b234c9dd30cc454d76fa11973e4858c6116e366
Sangram Khandagale <160144801+SangramKhandagale@users.noreply.github.com>
2025-10-24 08:10:17 +0530

    Add binary search implementation in Q26.java

--- Arrays/Q26.java
+++ Arrays/Q26.java
@@ -4,6 +4,7 @@ public class Q26{
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int key = 5;
+        
 
         int result = binarySearch(arr, 0, arr.length - 1, key);
 
