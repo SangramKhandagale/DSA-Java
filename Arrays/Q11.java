c781cb42823e31ff734c5f2a91143d6ccfdea646
Sangram Khandagale <160144801+SangramKhandagale@users.noreply.github.com>
2025-11-22 20:47:23 +0530

    Update Q11.java

--- Arrays/Q11.java
+++ Arrays/Q11.java
@@ -3,6 +3,7 @@ public class Q11{
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         int n=in.nextInt();
+        
         int arr[]=new int[n];
         int a[]=new int[n];
         for(int i=0;i<n;i++){
