ed467e4458230df68bb7392bc7bd0282873b0039
Sangram Khandagale <khandagalesangram26@gmail.com>
2025-12-02 15:31:03 +0530

    Lets goo

---
+++ Revision/Q1.java
@@ -0,0 +1,22 @@
+package Revision;
+
+import java.util.*;
+public class Q1{
+    public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+int n=in.nextInt();
+String name="Venom";
+Name(n,name);
+
+    }
+
+    public static void Name(int n,String name){
+if(n==1){
+  System.out.println(name);
+}else{
+   System.out.println(name);
+   Name(n-1,name);
+}
+
+    }
+}
