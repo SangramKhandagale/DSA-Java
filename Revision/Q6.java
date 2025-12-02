bb9e9eb496afed94c7ff49d0a37eba4e39f66d59
Sangram Khandagale <khandagalesangram26@gmail.com>
2025-12-02 16:30:03 +0530

    Q6 done

---
+++ Revision/Q6.java
@@ -0,0 +1,39 @@
+package Revision;
+import java.util.*;
+
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int arr[] = new int[n];
+
+        // take input
+        for (int k = 0; k < n; k++) {
+            arr[k] = in.nextInt();
+        }
+
+        int i = 0;
+        int j = n - 1;
+
+        int arr2[] = Reverse(arr, i, j);
+
+        // print reversed array
+        for (int x : arr2) {
+            System.out.print(x + " ");
+        }
+    }
+
+    public static int[] Reverse(int arr[], int i, int j) {
+
+        if (i >= j) {
+            return arr;
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        return Reverse(arr, i + 1, j - 1);
+    }
+}
