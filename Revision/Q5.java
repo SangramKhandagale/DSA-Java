27670ca527756e1104487161e143b60b1ba5fb88
Sangram Khandagale <khandagalesangram26@gmail.com>
2025-12-02 16:17:02 +0530

    Q5 done

---
+++ Revision/Q5.java
@@ -0,0 +1,19 @@
+package Revision;
+import java.util.*;
+class Q5{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+       Q5 sol=new Q5();
+       int result=sol.fact(n);
+       System.out.println(result);
+    }
+
+    public int fact(int n){
+        if(n==0){
+            return 1;
+        }else{
+            return n*fact(n-1);
+        }
+    }
+}
