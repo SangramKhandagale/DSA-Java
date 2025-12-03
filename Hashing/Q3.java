9324399f01651b3f0601714880d7c695dce1fa8b
Sangram Khandagale <khandagalesangram26@gmail.com>
2025-12-03 19:50:41 +0530

    Anagrams done

---
+++ Hashing/Q3.java
@@ -0,0 +1,31 @@
+package Hashing;
+import java.util.*;
+public class Q3{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        String s1=in.nextLine();
+        String s2=in.nextLine();
+
+        HashMap <Character,Integer> map1=new HashMap<>();
+        HashMap <Character,Integer> map2=new HashMap<>();
+
+        for(int i=0;i<s1.length();i++){
+            map1.put(s1.charAt(i),map1.getOrDefault(s1.charAt(i),0)+1);
+        }
+
+         for(int i=0;i<s2.length();i++){
+            map2.put(s2.charAt(i),map2.getOrDefault(s2.charAt(i),0)+1);
+        }
+
+        boolean result=false;
+
+if(map1.equals(map2)){
+    result=true;
+}else{
+    result=false;
+}
+
+System.out.println(result);
+    }
+}
