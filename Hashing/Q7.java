2240e8f9beccdbe357693922389f7476183e834b
Sangram Khandagale <khandagalesangram26@gmail.com>
2025-12-03 22:45:19 +0530

    HashSet concept done

---
+++ Hashing/Q7.java
@@ -0,0 +1,35 @@
+package Hashing;
+import java.util.*;
+public class Q7{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+int n=in.nextInt();
+int m=in.nextInt();
+int arr1[]=new int[n];
+int arr2[]=new int[m];
+
+for(int i=0;i<n;i++){
+    arr1[i]=in.nextInt();
+}
+
+for(int i=0;i<m;i++){
+    arr2[i]=in.nextInt();
+}
+
+HashSet<Integer> set=new HashSet<>();
+
+for(int i=0;i<n;i++){
+    set.add(arr1[i]);
+}
+
+HashSet<Integer> result=new HashSet<>();
+
+for(int i=0;i<m;i++){
+if(set.contains(arr2[i])){
+    result.add(arr2[i]);
+}
+}
+
+System.out.println(result);
+    }
+}
