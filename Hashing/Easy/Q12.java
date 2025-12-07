af411d4cebcf8821ba2177a7ed2695966dfd1eb7
Sangram Khandagale <khandagalesangram26@gmail.com>
2025-12-07 23:35:10 +0530

    commit

---
+++ Hashing/Easy/Q12.java
@@ -0,0 +1,38 @@
+package Hashing;
+import java.util.*;
+public class Q12{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String s1=in.next();
+        String s2=in.next();
+ArrayList<String> list=new ArrayList<>();
+        permute("",s1,list);
+
+        System.out.println(list);
+
+        boolean result=false;
+
+        for(int i=0;i<list.size();i++){
+            String per=list.get(i);
+           if(s2.contains(per)){
+            result=true;
+           }
+        }
+
+        System.out.println(result);
+    }
+
+    public static void permute(String prefix,String remaining,ArrayList<String> list){
+        if(remaining.length()==0){
+            list.add(prefix);
+        }
+
+        for(int i=0;i<remaining.length();i++){
+        char ch=remaining.charAt(i);
+        String newPrefix=prefix+ch;
+        String newRemaining=remaining.substring(0,i)+remaining.substring(i+1);
+
+        permute(newPrefix, newRemaining, list);
+        }
+    }
+}
---
+++ Hashing/Easy/Q16.java
@@ -0,0 +1,45 @@
+package Hashing;
+import java.util.*;
+
+public class Q16 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) arr[i] = in.nextInt();
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int x : arr) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        // Convert map to list (required for sorting)
+        List<Map.Entry<Integer,Integer>> list = new ArrayList<>(map.entrySet());
+
+        // bubble sort (NOOB FRIENDLY)
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i+1; j < list.size(); j++) {
+
+                // swap if frequency is smaller
+                if (list.get(i).getValue() < list.get(j).getValue()) {
+                    Map.Entry<Integer,Integer> temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+
+        // print sorted result in required format
+        for (Map.Entry<Integer,Integer> e : list) {
+            int val = e.getKey();
+            int freq = e.getValue();
+
+            for (int k = 0; k < freq; k++) {
+                System.out.print(val + " ");
+            }
+        }
+    }
+}
