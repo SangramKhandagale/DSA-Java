e84af55f15260df1881ac514198555965dbdf063
Sangram Khandagale <khandagalesangram26@gmail.com>
2025-12-07 23:17:23 +0530

    Done with today

--- Hashing/Medium/Q3.java
+++ Hashing/Medium/Q3.java
@@ -14,7 +14,6 @@ public class Q3{
         int m = in.nextInt();
 
         List<List<Integer>> list = new ArrayList<>();
-
         for(int i = 0; i < n; i++){
             for(int j = i+1; j < n; j++){
                 for(int k = j+1; k < n; k++){
