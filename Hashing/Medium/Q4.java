c4659138267fccfca6cde4e8d21833daed7d3c44
Sangram Khandagale <khandagalesangram26@gmail.com>
2025-12-08 20:32:04 +0530

    Changes

--- Hashing/Medium/Q4.java
+++ Hashing/Medium/Q4.java
@@ -5,9 +5,11 @@ public static void main(String[] args) {
 Scanner in=new Scanner(System.in);
 int n=in.nextInt();
 int arr[]=new int[n];
-for(int i=0;i<n;i++){
-arr[i]=in.nextInt();
-}
+
+    for(int i=0;i<n;i++){
+    arr[i]=in.nextInt();
+    }
+    
     int k=in.nextInt();
 
     int left=0, right=k-1;
