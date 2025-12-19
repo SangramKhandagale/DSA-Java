6be3971068281086860477d9a2b12ed932bf2360
Sangram Khandagale <khandagalesangram26@gmail.com>
2025-12-19 17:33:50 +0530

    Huhhhh

--- Hashing/Medium/Q6.java
+++ Hashing/Medium/Q6.java
@@ -1,6 +1,7 @@
+package Hashing.Medium;
 import java.util.*;
 
-public class LongestEqual012_Noob {
+public class Q6 {
     public static void main(String[] args) {
 
         int[] arr = {0,1,0,2,0,1,2};
