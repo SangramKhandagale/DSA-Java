97919f20ceee0c70c09c362af88610775cc4ab42
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-01-02 16:47:57 +0530

    Push

---
+++ Hashing/Medium/LargestZeroSumSubarray.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class LargestZeroSumSubarray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            if (sum == 0) {
+                maxLen = i + 1;
+            }
+
+            if (map.containsKey(sum)) {
+                maxLen = Math.max(maxLen, i - map.get(sum));
+            }
+          
+            else {
+                map.put(sum, i);
+            }
+        }
+
+        System.out.println(maxLen);
+        sc.close();
+    }
+}
