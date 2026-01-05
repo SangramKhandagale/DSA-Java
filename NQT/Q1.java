6e93aee7c24f7c1cfd63a546cca01047fc0e1607
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-01-05 22:13:43 +0530

    Done with the 1st question

---
+++ NQT/Q1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Q1 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (map.get(str.charAt(i)) == 1) {
+                System.out.println(str.charAt(i));
+                return;
+            }
+        }
+
+        System.out.println(-1);
+    }
+}
