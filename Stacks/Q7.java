56ac760957bb55f8bd8daaad61aa927a267d43d7
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-01-20 22:04:58 +0530

    Done for the day

---
+++ Stacks/Q7.java
@@ -0,0 +1,29 @@
+package Stacks;
+public class Q7 {
+    public String removeKdigits(String num, int k) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : num.toCharArray()) {
+            while (sb.length() > 0 && k > 0 && sb.charAt(sb.length() - 1) > c) {
+                sb.deleteCharAt(sb.length() - 1);
+                k--;
+            }
+            sb.append(c);
+        }
+
+        // Remove remaining digits if k > 0
+        while (k > 0 && sb.length() > 0) {
+            sb.deleteCharAt(sb.length() - 1);
+            k--;
+        }
+
+        // Remove leading zeros
+        int idx = 0;
+        while (idx < sb.length() && sb.charAt(idx) == '0') {
+            idx++;
+        }
+
+        String result = sb.substring(idx);
+        return result.isEmpty() ? "0" : result;
+    }
+}
