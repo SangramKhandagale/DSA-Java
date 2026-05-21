58072bdd694fc6fc2a85733179a11d81bfbc1e23
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-05-21 22:34:34 +0530

    Done with 7th question for the day

---
+++ Numbers/Q31.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Q31{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int sq=n*n;
+        int sum=0;
+        while(sq>0){
+            int r=sq%10;
+            sum=sum+r;
+            sq=sq/10;
+        }
+
+        if(sum==n){
+            System.out.println("Yes");
+        }else{
+            System.out.println("no");
+        }
+    }
+}
