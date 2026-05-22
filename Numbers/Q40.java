88373a86c1fa7559c8f8e0e47d5d25000564ea80
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-05-22 15:54:09 +0530

    Done with 6th question of the day

--- Numbers/Q40.java
+++ Numbers/Q40.java
@@ -2,6 +2,19 @@ import java.util.*;
 public class Q40{
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        
+        int n=in.nextInt();
+
+        int sum=0;
+        while(n>0){
+            int r=n%10;
+            sum=sum+r;
+            n=n/10;
+        }
+
+        if(n%sum==0){
+            System.out.println("Harshad");
+        }else{
+            System.out.println("Not Harshad");
+        }
     }
 }
