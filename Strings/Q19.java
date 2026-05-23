465f3b8119b7e213d3527a72f1efde12fc3949eb
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-05-23 20:08:55 +0530

    Done with the 6th question for the day

---
+++ Strings/Q19.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Q19{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < str.length(); i++){
+            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
+        }
+
+       
+        for(int i = 0; i < str.length(); i++){
+            if(map.get(str.charAt(i)) == 1){
+                System.out.println(str.charAt(i));
+                return; 
+            }
+        }
+
+      
+        System.out.println(-1);
+    }
+}
