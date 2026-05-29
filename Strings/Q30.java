6cdcf3e56092b09547b1710e4e67bd38e12366a3
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-05-29 22:58:12 +0530

    Done with 3rd question for the day

--- Strings/Q30.java
+++ Strings/Q30.java
@@ -1,40 +1,47 @@
 import java.util.*;
-public class Q30{
-    public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int n=in.nextInt();
-        
-        String arr[]=new String[n];
 
-        for(int i=0;i<n;i++){
-            arr[i]=in.next();
+public class Q30 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        String arr[] = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.next();
         }
 
-        String new_str="";
+        String new_str = "";
 
-     boolean result=true;
-    int k=0;
-    int count=0;
+        boolean result = true;
+        int k = 0;
 
-     while(result==true){
-        char ch=arr[0].charAt(k);
+        while (result == true) {
 
-        for(int i=0;i<n;i++){
-            if(arr[i].charAt(k)==ch){
-                continue;
-            }else{
-              result=false;
+            if (k >= arr[0].length()) {
                 break;
             }
-        }
 
+            char ch = arr[0].charAt(k);
 
- if (result) {
+            for (int i = 0; i < n; i++) {
+
+                if (k < arr[i].length() && arr[i].charAt(k) == ch) {
+                    continue;
+                } else {
+                    result = false;
+                    break;
+                }
+            }
+
+            if (result) {
                 new_str = new_str + ch;
                 k++;
             }
-     }
+        }
 
-     System.out.println(new_str);
+        System.out.println(new_str);
     }
 }
