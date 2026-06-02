2768dd2a82d72c11f85d676421b5fb6bea6aae81
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-06-02 12:42:28 +0530

    Done with 2nd question for the day

---
+++ Arrays/Q30.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Q30{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int arr[]=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+
+        int count=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(arr[i]>arr[j]){
+                 count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
