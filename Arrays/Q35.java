64e6db64a7399408659a4799c78e9de187801677
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-06-03 15:30:54 +0530

    Done with 3rd question for the day

---
+++ Arrays/Q35.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Q35{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int k=in.nextInt();
+
+int arr[]=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+
+         int count=0;
+
+         for(int i=0;i<n;i++){
+            int result=1;
+            for(int j=i;j<n;j++){
+           result=result*arr[j];
+
+           if(result==k){
+            count++;
+           }
+            }
+
+
+         }
+
+         System.out.println(count);
+
+    }
+}
