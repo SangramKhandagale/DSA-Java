c795a5e5fa345a84b05c641220b7c69fce41247f
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-06-30 19:43:04 +0530

    Done with 2nd question for the day

---
+++ Endgame/Q2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Q2{
+public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+int n=in.nextInt();
+
+String str=String.valueOf(n);
+int i=0,j=str.length()-1;
+boolean result=true;
+
+while(i<j){
+if(str.charAt(i)==str.charAt(j)){
+i++;
+j--;
+}else{
+result=false;
+break;
+}
+}
+
+System.out.println(result);
+
+}
+}
