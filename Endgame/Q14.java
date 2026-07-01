20afc1bd6e5a9ddadb1086ee6cee9f3aba3dc496
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-07-01 20:28:12 +0530

    Done with 7 questions!!!!

---
+++ Endgame/Q14.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Q14{
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+
+          map.put(arr[i],map.getOrDefault(arr[i], 0)+1);
+
+        }
+
+        for(int i=0;i<arr.length;i++){
+            if(map.get(arr[i])>1){
+                System.out.println(arr[i]);
+                break;
+            }
+        }
+    }
+}
