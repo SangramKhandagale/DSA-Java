28953bbf215715e7de00f184c5d52598f34c7a63
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-07-04 21:03:25 +0530

    Dnne for the day

---
+++ Endgame/Q30.java
@@ -0,0 +1,36 @@
+package Endgame;
+import java.util.*;
+public class Q30{
+public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+
+int n=in.nextInt();
+int arr[]=new int[n];
+int k=in.nextInt();
+
+for(int i=0;i<n;i++){
+arr[i]=in.nextInt();
+}
+
+HashMap<Integer,Integer> map=new HashMap<>();
+
+map.put(0,1);
+
+int sum=0,count=0;
+
+for(int i=0;i<n;i++){
+
+sum=sum+arr[i];
+
+if(map.containsKey(sum-k)){
+count=count+map.get(sum-k);
+}
+
+map.put(sum,map.getOrDefault(sum,0)+1);
+
+}
+
+System.out.println(count);
+
+}
+}
