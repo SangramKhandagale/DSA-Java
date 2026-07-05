67e0159655ed62a60b5776ae6f46b60e90048b27
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-07-05 15:29:19 +0530

    Done with the 4rd

---
+++ Endgame/Q34.java
@@ -0,0 +1,18 @@
+package Endgame;
+import java.util.*;
+public class Q33{
+public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+String str1=in.next();
+String str2=in.next();
+
+String str=str1+str1;
+
+if(str.contains(str2)){
+System.out.println("Yes");
+}else{
+System.out.println("No");
+}
+
+}
+}
