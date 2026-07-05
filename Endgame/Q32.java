e0c265e2b7c02f85c071f6542dafee4103b26187
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-07-05 13:26:08 +0530

    Done with the 2nd question

---
+++ Endgame/Q32.java
@@ -0,0 +1,23 @@
+package Endgame;
+import java.util.*;
+public class Q32{
+public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+int n=in.nextInt();
+
+int sum=0;
+
+for(int i=1;i<n;i++){
+if(n%i==0){
+sum=sum+i;
+}
+}
+
+if(sum==n){
+System.out.println("Perfect");
+}else{
+System.out.println("Not Perfect");
+}
+
+}
+}
