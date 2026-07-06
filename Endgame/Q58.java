a63cfd0a45b389d834948ffae1d60f8680684d08
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-07-06 16:48:54 +0530

    Done with 8 questions

---
+++ Endgame/Q58.java
@@ -0,0 +1,29 @@
+package Endgame;
+import java.util.*;
+public class Q58{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int k=in.nextInt();
+
+        int arr[]=new int[n];
+
+        ArrayList<Integer> list=new ArrayList<>();
+    
+    for(int i=0;i<n;i++){
+        arr[i]=in.nextInt();
+    }
+
+    for(int i=0;i<=n-k;i++){
+        int max=arr[i];
+        for(int j=i;j<i+k;j++){
+        max=Math.max(arr[j],max);
+        }
+
+        list.add(max);
+    }
+
+    System.out.println(list);
+
+    }
+}
