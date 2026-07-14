323481eed0ed6bb9b517c56a3b98045f5b5454ea
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-07-14 20:12:10 +0530

    Done with the 1st for the day

---
+++ Endgame/Q57.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Q57 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int x = in.nextInt();
+        int y = in.nextInt();
+
+        int ans = Integer.MAX_VALUE;
+
+        int maxVans = (n + 99) / 100;
+
+        for(int vans = 0; vans <= maxVans; vans++) {
+
+            int covered = vans * 100;
+
+            int remaining = Math.max(0, n - covered);
+
+            int cars = (remaining + 3) / 4;
+
+            int pollution = vans * y + cars * x;
+
+            ans = Math.min(ans, pollution);
+        }
+
+        System.out.println(ans);
+    }
+}
