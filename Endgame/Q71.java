f99da812efbe468456ec95b18031bca2537a427b
Sangram Khandagale <khandagalesangram26@gmail.com>
2026-07-23 17:25:19 +0530

    Done with the 4th

---
+++ Endgame/Q71.java
@@ -0,0 +1,28 @@
+package Endgame;
+import java.util.*;
+import java.util.Stack;
+public class Q71{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        Stack<String> st=new Stack<>();
+
+        for(int i=0;i<n;i++){
+            st.push(in.next());
+        }
+
+        int m=in.nextInt();
+
+        for(int i=0;i<m;i++){
+            st.pop();
+        }
+
+    if(st.isEmpty()){
+        System.out.println(true+" "+0+" "+0);
+    }else{
+         System.out.println(false+" "+st.peek()+" "+st.size());
+    }
+
+     
+    }
+}
